e5bfaa7f0529710ac2448099cb7ab879d7e399bd
azubkovas <arsenijus.zubkovas@gmail.com>
2025-01-29 02:09:01 +0000

    Updated GumTree client to default to srcml parser for supported filetypes.

--- src/main/java/bachelors/project/Main.java
+++ src/main/java/bachelors/project/Main.java
@@ -5,8 +5,7 @@ import bachelors.project.change.finder.NonEssThisAdditionToFieldFinder;
 import bachelors.project.change.finder.NonEssThisRemovalFromFieldFinder;
 import bachelors.project.change.finder.NonEssVariableRenameFinder;
 import com.github.gumtreediff.actions.model.Action;
-import bachelors.project.util.GumtreeClient;
-import com.github.gumtreediff.gen.srcml.SrcmlJavaTreeGenerator;
+import bachelors.project.util.GumTreeClient;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -16,26 +15,26 @@ public class Main {
     public static void main(String[] args) throws IOException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
         System.out.println("Variable rename casualties:");
         System.out.println("Java");
-        printChanges(new NonEssVariableRenameFinder().findChanges(GumtreeClient.getDiffData("data/rename_casualties/BeforeVariableRename.java",
-                "data/rename_casualties/AfterVariableRename.java", SrcmlJavaTreeGenerator.class)));
+        printChanges(new NonEssVariableRenameFinder().findChanges(GumTreeClient.getDiffData("data/rename_casualties/BeforeVariableRename.java",
+                "data/rename_casualties/AfterVariableRename.java")));
         System.out.println("C++");
-        printChanges(new NonEssVariableRenameFinder().findChanges(GumtreeClient.getDiffData("data/rename_casualties/BeforeVariableRename.cpp",
+        printChanges(new NonEssVariableRenameFinder().findChanges(GumTreeClient.getDiffData("data/rename_casualties/BeforeVariableRename.cpp",
                 "data/rename_casualties/AfterVariableRename.cpp")));
 
         System.out.println("\nMethod rename casualties:");
         System.out.println("Java");
-        printChanges(new NonEssMethodRenameFinder().findChanges(GumtreeClient.getDiffData("data/rename_casualties/BeforeMethodRename.java",
-                "data/rename_casualties/AfterMethodRename.java", SrcmlJavaTreeGenerator.class)));
+        printChanges(new NonEssMethodRenameFinder().findChanges(GumTreeClient.getDiffData("data/rename_casualties/BeforeMethodRename.java",
+                "data/rename_casualties/AfterMethodRename.java")));
         System.out.println("C++");
-        printChanges(new NonEssMethodRenameFinder().findChanges(GumtreeClient.getDiffData("data/rename_casualties/BeforeMethodRename.cpp",
+        printChanges(new NonEssMethodRenameFinder().findChanges(GumTreeClient.getDiffData("data/rename_casualties/BeforeMethodRename.cpp",
                 "data/rename_casualties/AfterMethodRename.cpp")));
 
         System.out.println("\nTrivial this keyword removal from field access changes:");
-        printChanges(new NonEssThisRemovalFromFieldFinder().findChanges(GumtreeClient.getDiffData("data/trivial_keywords/WithThisKeyword.java",
+        printChanges(new NonEssThisRemovalFromFieldFinder().findChanges(GumTreeClient.getDiffData("data/trivial_keywords/WithThisKeyword.java",
                 "data/trivial_keywords/WithoutThisKeyword.java")));
 
         System.out.println("\nTrivial this keyword addition to field access changes:");
-        printChanges(new NonEssThisAdditionToFieldFinder().findChanges(GumtreeClient.getDiffData("data/trivial_keywords/WithoutThisKeyword.java",
+        printChanges(new NonEssThisAdditionToFieldFinder().findChanges(GumTreeClient.getDiffData("data/trivial_keywords/WithoutThisKeyword.java",
                 "data/trivial_keywords/WithThisKeyword.java")));
 
     }
---
+++ src/main/java/bachelors/project/util/GumTreeClient.java
@@ -0,0 +1,52 @@
+package bachelors.project.util;
+
+import com.github.gumtreediff.actions.EditScriptGenerator;
+import com.github.gumtreediff.actions.SimplifiedChawatheScriptGenerator;
+import com.github.gumtreediff.client.Run;
+import com.github.gumtreediff.gen.TreeGenerator;
+import com.github.gumtreediff.gen.TreeGenerators;
+import com.github.gumtreediff.gen.srcml.SrcmlCTreeGenerator;
+import com.github.gumtreediff.gen.srcml.SrcmlCppTreeGenerator;
+import com.github.gumtreediff.gen.srcml.SrcmlCsTreeGenerator;
+import com.github.gumtreediff.gen.srcml.SrcmlJavaTreeGenerator;
+import com.github.gumtreediff.matchers.MappingStore;
+import com.github.gumtreediff.matchers.Matcher;
+import com.github.gumtreediff.matchers.Matchers;
+import com.github.gumtreediff.tree.Tree;
+
+import java.io.IOException;
+
+public class GumTreeClient {
+    // Assumption: before and after files are of the same type.
+    public static DiffData getDiffData(String beforeFilePath, String afterFilePath) throws IOException {
+        TreeGenerator generator;
+        if (beforeFilePath.endsWith(".java")) {
+            generator = new SrcmlJavaTreeGenerator();
+        }
+        else if (beforeFilePath.endsWith(".cpp")) {
+            generator = new SrcmlCppTreeGenerator();
+        }
+        else if (beforeFilePath.endsWith(".c") || beforeFilePath.endsWith(".h")) {
+            generator = new SrcmlCTreeGenerator();
+        }
+        else if (beforeFilePath.endsWith(".cs")) {
+            generator = new SrcmlCsTreeGenerator();
+        }
+        else {
+            Run.initGenerators();
+            generator = TreeGenerators.getInstance().get(beforeFilePath);
+        }
+        Tree before = generator.generateFrom().file(beforeFilePath).getRoot();
+        Tree after = generator.generateFrom().file(afterFilePath).getRoot();
+        return getDiffData(beforeFilePath, afterFilePath, before, after);
+    }
+
+    private static DiffData getDiffData(String beforeFilePath, String afterFilePath, Tree before, Tree after) {
+        Matcher defaultMatcher = Matchers.getInstance().getMatcher();
+        MappingStore mappings = defaultMatcher.match(before, after);
+        EditScriptGenerator editScriptGenerator = new SimplifiedChawatheScriptGenerator();
+        return new DiffData(before, after, beforeFilePath, afterFilePath, mappings, editScriptGenerator.computeActions(mappings));
+    }
+
+}
+

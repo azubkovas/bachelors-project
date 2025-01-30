0a890c7add108a3e315a7383cd178570574a35f0
azubkovas <arsenijus.zubkovas@gmail.com>
2025-01-30 22:49:30 +0000

    Changed dummy main implementation to a specific example.

--- src/main/java/bachelors/project/Main.java
+++ src/main/java/bachelors/project/Main.java
@@ -1,6 +1,7 @@
 package bachelors.project;
 
 import bachelors.project.change.finder.ChangeFinder;
+import bachelors.project.change.finder.NonEssVariableRenameFinder;
 import bachelors.project.util.DiffData;
 import bachelors.project.util.GumTreeClient;
 import com.github.gumtreediff.actions.model.Action;
@@ -10,11 +11,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String beforeFilePath = "", afterFilePath = "";
+        String beforeFilePath = "src/test/data/rename_casualties/BeforeVariableRename.java";
+        String afterFilePath = "src/test/data/rename_casualties/AfterVariableRename.java";
         DiffData diffData = GumTreeClient.getDiffData(beforeFilePath, afterFilePath);
-        ChangeFinder changeFinder = null;
-        List<Action> changes = changeFinder.findChanges(diffData);
-        printChanges(changes);
+        ChangeFinder changeFinder = new NonEssVariableRenameFinder();
+        List<Action> nonEssChanges = changeFinder.findChanges(diffData);
+        printChanges(nonEssChanges);
     }
 
     private static void printChanges(List<Action> changes) {
--- src/test/data/rename_casualties/AfterVariableRename.java
+++ src/test/data/rename_casualties/AfterVariableRename.java
@@ -2,7 +2,7 @@ public class Program {
     public static void main(String[] args) {
         int x = 2;
         int y1 = 2;
-        int z = y1 + y1;
+        int z = y1 * y1;
         System.out.println(x + z);
     }
 }
